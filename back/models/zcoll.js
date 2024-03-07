d39c8b5d89fca4588f9d11d309bf22684cb145f7
Terence <terenceraja@hotmail.fr>
2024-03-07 18:00:07 +0100

    table zcoll added

---
+++ back/models/zcoll.js
@@ -0,0 +1,43 @@
+module.exports = (sequelize, DataTypes) => {
+  const zcoll = sequelize.define(
+    "zcoll",
+    {
+      IdColl: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      Name: {
+        type: DataTypes.CHAR(50),
+        allowNull: false,
+      },
+      Surname: {
+        type: DataTypes.CHAR(50),
+        allowNull: false,
+        collate: "utf8_general_ci",
+      },
+      Color: {
+        type: DataTypes.CHAR(50),
+        allowNull: false,
+        collate: "utf8_general_ci",
+      },
+      TimeStampCreation: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW, // Use Sequelize's NOW function
+      },
+      TimeStampModification: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW, // Use Sequelize's NOW function
+      },
+    },
+    {
+      freezeTableName: true, // prevent Sequelize from pluralizing the table name
+      timestamps: false, // disable timestamps (createdAt and updatedAt)
+      tableName: "zcoll", // explicitly specify the table name
+    }
+  );
+
+  return zcoll;
+};
