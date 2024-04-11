cefad50e9ac2db3c5260e8d648d653726d5187fc
Terence <terenceraja@hotmail.fr>
2024-04-11 11:08:18 +0200

    socket fix

--- back/app.js
+++ back/app.js
@@ -1,22 +1,23 @@
-var express = require("express");
-var path = require("path");
-var http = require("http");
+// In app.js
 
-
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const { Server } = require("socket.io");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 require("dotenv").config();
 
-var indexRouter = require("./routes/index");
-var docRouter = require("./routes/doc");
-var collabsRouter = require("./routes/collabs");
-var newsRouter = require("./routes/news");
-var iisRouter = require("./routes/iisTest");
-var messageRouter = require("./routes/message");
-var chatRouter = require("./routes/chat");
-var filemakerRouter = require("./routes/filemaker");
+const indexRouter = require("./routes/index");
+const docRouter = require("./routes/doc");
+const collabsRouter = require("./routes/collabs");
+const newsRouter = require("./routes/news");
+const iisRouter = require("./routes/iisTest");
+const messageRouter = require("./routes/message");
+const chatRouter = require("./routes/chat");
+const filemakerRouter = require("./routes/filemaker");
 
-var app = express();
+const app = express();
 
 const cors = require("cors");
 // Allow requests from both http://localhost:5173 and http://localhost:5500
@@ -24,7 +25,7 @@ app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); ////////////////////////////////////// FALSE ORIGINALY
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -37,7 +38,146 @@ app.use("/chat", chatRouter);
 app.use("/iis", iisRouter);
 app.use("/filemaker", filemakerRouter);
 
-// ///////// IIS CONFIG ///////////
-app.listen(process.env.PORT);
 
-module.exports = { app, http }; // Export both the app and the http server
+//// // // // // // // // // // // // // // // // // ////// // // // // // // // // // // // // // // // // // // // // // ///
+//// // // // // // // // // // // // // // // // // //  SOCKET CONFIG // / // // // // // // // // // // // // // // // // //
+//// // // // // // // // // // // // // // // // // ////// // // // // // // // // // // // // // // // // // // // // // ///
+
+
+// Create HTTP server
+const server = http.createServer(app);
+
+// Attach Socket.IO to the HTTP server
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173", "http://127.0.0.1:5500", "https://mykeeapp.keesystem.com"],
+    methods: ["GET", "POST"],
+  },
+});
+
+
+
+
+//// // // // // // // // // // // // // // // // // ////// // // // // // // // // // // // // // // // // // // // // // //
+//// // // // // // // // // // // // // // // // // //  SOCKET EVENTS  // // // // // // // // // // // // // // // // // //
+//// // // // // // // // // // // // // // // // // ////// // // // // // // // // // // // // // // // // // // // // // //
+
+
+// Socket.IO event handling
+let onlineUsers = [];
+io.on("connection", (socket) => {
+  console.log(`socket connection ${socket.id}`);
+
+  // JOIN CHAT/ROOM
+  socket.on("joinRoom", (IdChat) => {
+    console.log(IdChat);
+    console.log(`USER JOINED ROOM : ${IdChat}`);
+    socket.join(IdChat);
+    // socket.join([10]);
+  });
+
+  // SENDING MESSAGE
+  socket.on("sendMessage", (data) => {
+    console.log("THE ROOM TO SEND TO", data.messageToSend.IdChat);
+
+    // Get the list of sockets in the room
+    const socketsInRoom = io
+      .of("/")
+      .adapter.rooms.get(data.messageToSend.IdChat);
+
+    // Emit the message to all sockets in the room except the sender's socket
+    if (socketsInRoom) {
+      for (const socketId of socketsInRoom) {
+        if (socketId !== socket.id) {
+          io.to(socketId).emit("getMessage", data.messageToSend);
+        }
+      }
+    }
+  });
+
+  // DISCONNECT
+  socket.on("disconnect", () => {
+    console.log("socket disconnected", socket.id);
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+    console.log("onlineUsers", onlineUsers);
+    io.emit("getOnlineUsers", onlineUsers);
+  });
+
+  // ADD ONLINE USERS
+  socket.on("addNewUser", (userData) => {
+    console.log("USER DATA");
+    const existingUser = onlineUsers.find(
+      (user) => user.IdUser === userData.IdUser
+    );
+    if (existingUser) {
+      existingUser.socketId = socket.id;
+    } else {
+      onlineUsers.push({
+        IdUser: userData.IdUser,
+        Room: userData.room,
+        userType: userData.userType,
+        socketId: socket.id,
+      });
+    }
+    console.log("onlineUsers", onlineUsers);
+    io.emit("getOnlineUsers", onlineUsers);
+  });
+});
+
+
+
+//// // // // // // // // // // // // // // // // // ////// // // // // // // // // // // // // // // // // // // // // // //
+//// // // // // // // // // // // // // // // // // //  PORT CONFIG /  // // // // // // // // // // // // // // // // // //
+//// // // // // // // // // // // // // // // // // ////// // // // // // // // // // // // // // // // // // // // // // //
+
+// Listen on provided port
+const port = normalizePort(process.env.PORT || "4000");
+app.set("port", port);
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  console.log("Server is listening on " + bind);
+}
+
+module.exports = app;
